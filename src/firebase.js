45d32ca908758f37d28d8e981fe9bd8fc0a42dbc
Agustin <agu@cursosit.com>
2020-10-25 15:59:54 -0300

    create Chat, Sidebar components and same css

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDG0wRUvVT9VORdOBQfO3-S0vtTqBKRUKw",
+    authDomain: "whatsapp-mern-790ad.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-790ad.firebaseio.com",
+    projectId: "whatsapp-mern-790ad",
+    storageBucket: "whatsapp-mern-790ad.appspot.com",
+    messagingSenderId: "678488382268",
+    appId: "1:678488382268:web:9e95fbbb3ff972c38f5c39"
+  };
