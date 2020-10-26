1e82b6b708ebac71719de2ad94c7664a36eca5d1
Agustin <agu@cursosit.com>
2020-10-26 17:07:01 -0300

    add header chat container

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,38 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from 'react';
 import "./Chat.css"
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>I am the chat</h1>
+            <div className="chat__header">
+                <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined /> 
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile /> 
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert /> 
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                <p>
+                    <span className="chat__name">Sonny</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
         </div>
     )
 }
