6d3023e290986076483252d2e26010fc7fc25b40
Agustin <agu@cursosit.com>
2020-10-26 18:22:24 -0300

    add footer chat container & end frontend

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,5 @@
 import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from 'react';
 import "./Chat.css"
 
@@ -39,6 +39,14 @@ function Chat() {
                         {new Date().toUTCString()}
                     </span>
                 </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticon />
+                <form>
+                    <input placeholder="Type a message" type="text"/>
+                    <button type="submit">Send a message</button>
+                </form>
+                <Mic />
             </div>
         </div>
     )
