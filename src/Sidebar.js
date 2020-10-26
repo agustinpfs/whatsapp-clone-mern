3287e32768008229fda7816cc541864ed0a4bfdc
Agustin <agu@cursosit.com>
2020-10-26 15:07:53 -0300

    add sidebar rooms

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,7 @@ import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { Avatar, IconButton } from '@material-ui/core';
 import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
@@ -28,9 +29,12 @@ function Sidebar() {
                         <SearchOutlined />
                         <input placeholder="Search or start new chat" type="text"/>
                     </div>
-
                 </div>
-
+                <div className="sidebar__chats">
+                    <SidebarChat />
+                    <SidebarChat />
+                    <SidebarChat />
+                </div>
         </div>
     )
 }
